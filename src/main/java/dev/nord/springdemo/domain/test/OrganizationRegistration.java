f4dd034ece5b2d2ace57bd569ab642464fcc96b0
nordcave <nadraga.oleg@gmail.com>
2019-06-22 18:25:42 +0300

    SpringFormTags learning init

---
+++ src/main/java/dev/nord/springdemo/domain/test/OrganizationRegistration.java
@@ -0,0 +1,15 @@
+package dev.nord.springdemo.domain.test;
+
+public class OrganizationRegistration {
+	
+	private String orgName;
+
+	public String getOrgName() {
+		return orgName;
+	}
+
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+
+}
